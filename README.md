61bbf25e67bd3acabce3cddd50359f8ee16c00c5
MontherSaiid <saeedmundher@gmail.com>
2022-07-31 21:07:46 +0200

    update
    this is update fix some error

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# todolist
+## Todolist
+It is a small project that does some daily tasks.
+
+### Project Interface
+The interface consists of an input field and an add button.
+
+- Input field Through this field, the user can enter daily tasks.
+- The add button is through which tasks are added to the program, i.e. tasks are stored in the browser so that the tasks remain present if the browser is closed.
+
+### Technologies Used
+- Html
+- Css
+- JavaScript
+
+### Responsive
+responsive from Mobile devices to desktop devices.
+
--- js/script.js
+++ js/script.js
@@ -5,80 +5,112 @@ let display = document.querySelector('.display');
 let showError = document.querySelector('.error');
 
 
-let data = localStorage.getItem('tasks');
 
-if (data) {
-    console.log(JSON.parse(data))
-    let newArray = JSON.parse(data);
-    newArray.forEach(item => {
-        let row = document.createElement('div');
-        row.classList.add('row');
-        row.innerHTML = `
-                <h2 class="nametask">${item.task}</h2>
-                <div>
-                    <button class="btn btn__done">done</button>
-                    <button class="btn btn__delete">delete</button>
-                </div>`;
-        display.appendChild(row);
-        showError.style.display = 'none';
+// storage tasks
+let arrayTasks = [];
 
-    });
-}
-else {
-    showError.style.display = 'block';
+// call getData from localStorage
+getData();
+
+// check if there are any tasks in localStorage
+if (localStorage.getItem('tasks')) {
+    arrayTasks = JSON.parse(localStorage.getItem('tasks'));
 }
 
+
+// Add task
 btnAdd.addEventListener('click', function (e) {
     // remove reload from window
     e.preventDefault();
-
     if (taskInput.value !== '' && taskInput.value.length <= 50) {
-        // call function createElements
-        createElements();
-        // call function to save the data in localStorage
-        saveData();
+        addTask(taskInput.value);
+        taskInput.value = '';
     }
-
 });
 
-function createElements() {
+let nameTask = document.querySelector('.nametask');
 
+display.addEventListener('click', function (e) {
+    //hold the id if the button is clicked
+    let id = e.target.parentElement.parentElement.getAttribute('data-id');
+    if (e.target.classList.contains('btn__delete')) {
+        // check if there class thro to delete it
+        if (nameTask.classList.contains('thro')) {
+            // remove task from localStorage
+            removeTask(id);
+            // remove task from page
+            e.target.parentElement.parentElement.remove();
+        }
 
-    // Hold the value of field input
-    let task = taskInput.value;
-    // create the row
-    let row = document.createElement('div');
-    row.classList.add('row');
-    row.innerHTML = `
-        <h2 class="nametask">${task}</h2>
-        <div>
-            <button class="btn btn__done">done</button>
-            <button class="btn btn__delete">delete</button>
-        </div>`;
+    }
 
-    // append the row to the display
-    showError.style.display = 'none';
-    display.appendChild(row);
-}
+    if (e.target.classList.contains('btn__done')) {
+        e.target.classList.toggle('undo');
+        if (nameTask.parentElement.getAttribute('data-id') == id) {
+            nameTask.classList.toggle('thro');
+        }
 
-const saveData = () => {
+        e.target.innerHTML === 'done' ? e.target.innerHTML = 'undo' : e.target.innerHTML = 'done';
 
+    }
+});
+
+function addTask(task) {
     // create object to save the data
     let allData = {
-        task: taskInput.value,
-        id: Math.floor(Math.random() * 1000)
+        id: Math.floor(Math.random() * 100),
+        taskTitle: task,
+
     };
 
-    // get the data from localStorage
-    let newArray = JSON.parse(localStorage.getItem('tasks')) ?
-        JSON.parse(localStorage.getItem('tasks')) : [];
-
     // add the data to the array
-    newArray.push(allData);
+    arrayTasks.push(allData);
+    // create elements
+    createElements(arrayTasks);
+    // call function to save the data in localStorage
+    saveData(arrayTasks);
+    // reload for the page
+    location.reload();
+}
 
-    // save the data in localStorage
-    localStorage.setItem('tasks', JSON.stringify(newArray));
 
-    // clear the field input
-    taskInput.value = '';
+function createElements(allTasks) {
+    display.innerHTML = '';
+    // create the row
+    allTasks.forEach(task => {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        row.setAttribute('data-id', task.id);
+
+        row.innerHTML = `
+        <h2 class="nametask" data-id=${task.id}>${task.taskTitle}</h2>
+        <div>
+            <button class="btn btn__done" data-id=${task.id}>done</button>
+            <button class="btn btn__delete" data-id=${task.id}>delete</button>
+        </div>`;
+
+        showError.style.display = 'none';
+        // append the row to the display
+        display.appendChild(row);
+    });
+}
+
+function saveData(allTasks) {
+    localStorage.setItem('tasks', JSON.stringify(allTasks));
+}
+
+function getData() {
+    let data = localStorage.getItem('tasks');
+    if (data) {
+        let tasks = JSON.parse(data);
+        createElements(tasks);
+    }
+    showError.style.display = 'block';
+}
+
+
+function removeTask(id) {
+    arrayTasks = arrayTasks.filter((task) => task.id != id);
+    saveData(arrayTasks);
+
 }
